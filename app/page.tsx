f8b5234c23d284ace4092deba6fd6e16c1c251fa
achmaouiayoub120-beep <achmaouiayoub120@gmail.com>
2026-05-02 14:13:59 +0100

    Fix TypeScript error: allowFullScreen must be boolean

--- app/page.tsx
+++ app/page.tsx
@@ -402,7 +402,7 @@ export default function Home() {
                   width="100%" 
                   height="100%" 
                   style={{ border: 0 }} 
-                  allowFullScreen="" 
+                  allowFullScreen 
                   loading="lazy" 
                   referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
